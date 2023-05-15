65be0ea94bfdeccf83739f588b07c5c51de3c2db
Raoul <federico.raoul@gmail.com>
2023-05-15 11:20:05 +0200

    solo design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={"min-h-screen flex flex-col"}>
+        <Navbar/>
+        <div className='flex-grow'>
+          {children}
+        </div>
+        <Footer/>
+      </body>
     </html>
   )
 }
+
